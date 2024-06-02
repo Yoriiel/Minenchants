ba5d612d064fc9b8e5772ab5dedbae0b29eddad6
Yoriiel <yoriiel.gonzalez@gmail.com>
2024-06-02 18:22:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-//ReactDOM.render(
-//  <h1>Hello, world!</h1>,
-//  document.getElementById('contenedor')
-//);
+document.addEventListener("DOMContentLoaded", function() {
+    // Función para alternar la visibilidad de la lista de encantamientos
+    function toggleEnchantments(id, itemClass) {
+      const list = document.getElementById(id);
+      if (list) {
+        list.classList.toggle('hidden');
+        const itemImg = document.querySelector(itemClass);
+        if (itemImg) {
+          itemImg.classList.toggle('achicar');
+        }
+      }
+    }
+  
+    // Añadir el evento a todos los botones de "Ver"
+    const buttons = document.querySelectorAll('.rainbow-button');
+    buttons.forEach(button => {
+      button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('data-target');
+        const itemClass = this.getAttribute('data-item-class');
+        toggleEnchantments(targetId, itemClass);
+      });
+    });
+  });
